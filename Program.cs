da7f7cd3b22465f496b4ba5e6ba0135e782ae668
cngetich37 <cngetich37@gmail.com>
2026-04-03 12:31:28 +0300

    get new changes

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -14,7 +14,7 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
